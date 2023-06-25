1e40a2a1b17d73d95736968ebaa2034abc4a847f
noelia-paz <noeliapaz676@gmail.com>
2023-06-25 19:25:41 -0300

    styles form

--- src/app/Components/Form/Form.tsx
+++ src/app/Components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import emailjs from "@emailjs/browser";
+/*import emailjs from "@emailjs/browser";
 import React from "react";
 import swal from "sweetalert";
 import styles from "./Form.module.css";
@@ -57,6 +57,91 @@ function Form() {
         </div>
       </div>
     </form>
+  );
+}
+
+export default Form;*/
+
+import emailjs from "@emailjs/browser";
+import React from "react";
+import swal from "sweetalert";
+import styles from "./Form.module.css";
+import { motion } from "framer-motion";
+
+function Form() {
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_8awba0h",
+      "template_1dovcxn",
+      e.target,
+      "QvBuxbHtSWiZHY-0s"
+    );
+
+    swal({
+      text: "Se envio con exito!",
+      icon: "success",
+    }).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+    e.target.reset();
+  };
+  return (
+    <div className={styles.divForm} id="contactMe">
+      <motion.form
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className={styles.formulario}
+        onSubmit={sendEmail}
+      >
+        <h2 className={styles.h22}>Gracias por ponerte en contacto!</h2>
+        <div className={styles.inputcontainer}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nombre"
+            className={styles.input}
+            required
+          />
+        </div>
+        <div className={styles.inputcontainer}>
+          <input
+            type="text"
+            name="last"
+            placeholder="Apellido"
+            className={styles.input}
+            required
+          />
+        </div>
+        <div className={styles.inputcontainer}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Dejame tu email"
+            className={styles.input}
+            required
+          />
+        </div>
+        <div className={styles.inputcontainer}>
+          <textarea
+            placeholder="Mensaje"
+            name="message"
+            className={styles.inputTextarea}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.button}>
+          Enviar
+        </button>
+      </motion.form>
+    </div>
   );
 }
 
